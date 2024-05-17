009323e8bbfb65a1a3044fe9d89d9fe531c52e67
Tarek Al-Qarqaz <tbacode@outlook.com>
2024-05-16 22:13:32 -0500

    [change]: Made init more modular

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,13 +1,73 @@
 #!/bin/bash
 
-# Fill these in
-NODE_ID=
-NODE_PREFIX=
-WEBHOOK=
-MESSAGE_PREFIX="${NODE_PREFIX}${NODE_ID}"
-USERNAME=
-PASSWORD=
-ORIGINAL_USERNAME="ubuntu"
+# Init script. Designed to run on instance creation.
+# Required flags: -u -p -o -n - -w -m
+
+# Guard against running with regular permissions 
+if [ "$EUID" -ne 0 ]
+  then echo Please run this script as root or using sudo.
+  exit
+fi
+
+# Argument parser
+REQUIRED_ARGS=("USERNAME" "PASSWORD" "ORIGINAL_USERNAME" "NODE_PREFIX" "NODE_ID" "WEBHOOK" "MESSAGE_PREFIX")
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -u|--username)
+      USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--password)
+      PASSWORD="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--original-username)
+      ORIGINAL_USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n|--node-prefix)
+      NODE_PREFIX="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -i|--node-id)
+      NODE_ID="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -w|--webhook)
+      WEBHOOK="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -m|--message-prefix)
+      MESSAGE_PREFIX="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+# Exit if we are missing a required argument.
+for arg_name in "${REQUIRED_ARGS[@]}"; do
+  if [[ -z "${!arg_name}" ]]
+    then echo "Missing required argument: ${arg_name}"
+    exit
+  fi
+done
 
 # Update username and password
 curl https://raw.githubusercontent.com/tba-code/ldsmq/main/scripts/utils/change-default-user.sh | sudo bash -s -- \
